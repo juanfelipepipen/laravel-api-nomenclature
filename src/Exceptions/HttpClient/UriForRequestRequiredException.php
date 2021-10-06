07f4aa11f0ac886ad26c1c1d6be9f8cc11785dca
Juan Felipe Lopez Estrada <juan.lopez@wintesler.com>
2021-10-05 20:28:24 -0700

    Alpha 1

---
+++ src/Exceptions/HttpClient/UriForRequestRequiredException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Pipen\ApiTesting\Exceptions\HttpClient;
+
+use JetBrains\PhpStorm\Pure;
+
+class UriForRequestRequiredException extends \Exception
+{
+    /**
+     * Class constructor
+     */
+    #[Pure] public function __construct()
+    {
+        parent::__construct('The uri where the request will be sent is needed');
+    }
+}
